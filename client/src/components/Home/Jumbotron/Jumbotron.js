ee9583ae5ffe764b81dc96ae7b1809e08e4049b3
Artawood Chitamitara <artawood@airbears2-10-142-77-14.airbears2.1918.berkeley.edu>
2018-10-15 16:56:31 -0700

    added ds global styles

--- client/src/components/Home/Jumbotron/Jumbotron.js
+++ client/src/components/Home/Jumbotron/Jumbotron.js
@@ -2,15 +2,15 @@ import React from "react";
 import "./Jumbotron.css";
 
 const Jumbotron = () => (
-  <div className="jumbotron">
+  <div className="jumbotron bg-ds-white">
     <div className="container text-center">
       <h1>DevStart</h1>
       <h2>Helping begining developers get started</h2>
       <p>
-        <a className="btn btn-primary btn-lg" href="#" role="button">
+        <a className="btn btn-ds-primary btn-lg" href="#" role="button">
           I'm an entrepenuer
         </a>
-        <a className="btn btn-primary btn-lg" href="#" role="button">
+        <a className="btn btn-ds-primary btn-lg" href="#" role="button">
           I'm a starting developer
         </a>
       </p>
