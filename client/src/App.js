17430e30eb715aa3938f3eb1fbbd6992638fac16
Artawood Chitamitara <artawood@airbears2-10-142-76-149.airbears2.1918.berkeley.edu>
2018-10-17 16:35:05 -0700

    added routes to main pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,23 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from "./components/Home";
+//uncomment this below and the route when signup form is ready
+// import SignUpForm from "./components/SignUp/Form";
+import DeveloperDashboard from "./components/Developer/Dashboard";
+import SponsorDashboard from "./components/Sponsor/Dashboard";
 
 class App extends Component {
   render() {
-    return <Home />;
+    return (
+      <Router>
+        <div>
+          <Route exact path="/" component={Home} />
+          {/* <Route exact path="/signup" component={SignUpForm} /> */}
+          <Route path="/developer-dashboard" component={DeveloperDashboard} />
+          <Route path="/sponsor-dashboard" component={SponsorDashboard} />
+        </div>
+      </Router>
+    );
   }
 }
 
