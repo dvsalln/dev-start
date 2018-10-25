93df5572043fa976bce01869323b86627f287672
Doc Ellis <DAVROGALL@ICLOUD.COM>
2018-10-25 00:46:14 -0700

    sideDrawer working throughout

--- client/src/components/Sponsor/Dashboard/Dashboard.js
+++ client/src/components/Sponsor/Dashboard/Dashboard.js
@@ -1,23 +1,54 @@
 import React, { Component } from "react";
 import Header from "../../Global/Header";
-import ColumeOne from "./ColumnOne";
-import ColumnTwo from "./ColumnTwo";
-import ColumnThree from "./ColumnThree";
+import ColumnOne from "../Dashboard/ColumnOne";
+import ColumnTwo from "../Dashboard/ColumnTwo";
+import ColumnThree from "../Dashboard/ColumnThree";
 import "./Dashboard.css";
+import SideDrawer from "../../Global/SideDrawer";
+import Backdrop from "../../Global/Backdrop";
 
 
 class Dashboard extends Component {
+  state = {
+    sideDrawerOpen: false
+  };
+
+  drawerToggleClickHandler = () => {
+    this.setState(prevState => {
+      return { sideDrawerOpen: !prevState.sideDrawerOpen };
+    });
+  };
+
+  backdropClickHandler = () => {
+    this.setState({ sideDrawerOpen: false });
+  };
   render() {
+    let backdrop;
+    let sideDrawer;
+
+    if (this.state.sideDrawerOpen) {
+      sideDrawer = <SideDrawer />;
+
+      backdrop = <Backdrop click={this.backdropClickHandler} />;
+    }
+
     return (
-      <div>
-        <Header />
-        <div className="container dashboard">
-          <div className="row">
-            <ColumeOne />
+      <div class="container-flex">
+      <div style={{ height: '100%' }}>
+        <div className="wrapper">
+          <div className="row dev-db-row">
+            <Header drawerClickHandler={this.drawerToggleClickHandler}/>
+            <SideDrawer show={this.state.sideDrawerOpen} />
+            {backdrop}
+            {sideDrawer}
+            <ColumnOne />
+
             <ColumnTwo />
+
             <ColumnThree />
           </div>
         </div>
+      </div>
       </div>
     );
   }
