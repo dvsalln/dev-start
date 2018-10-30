90b107b8092fb534911af9b72e6da122749e26f9
AAIO1992 <takahiro04kawaguchi13@gmail.com>
2018-10-30 01:21:28 -0700

    I added brunch earlier..but it's not on repo..?

---
+++ client/src/components/SignUp/AccountDataService/AccountDataService.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import axios from "axios";
+
+class AccountDataService {
+  sendData(data) {
+    axios
+      .post("http://localhost:3090/api/developer/account/create", {
+        data: data
+      })
+      .then(function(res) {
+        console.log(res);
+      })
+      .catch(function(err) {
+        console.log(err);
+      });
+  }
+}
+
+export default AccountDataService;
