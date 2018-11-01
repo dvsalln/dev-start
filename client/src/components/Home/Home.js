5f20993d509c47b8b7ba6801de80f85602b98a55
Artawood Chitamitara <artawood@Artawoods-MacBook-Pro.local>
2018-11-01 12:03:38 -0700

    restored global css

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -2,6 +2,7 @@ import React, { Component, Fragment } from "react";
 import HomeHeader from "../Global/HomeHeader";
 import Jumbotron from "../Home/Jumbotron";
 import "./Home.css";
+import "../../css/ds-global.css";
 
 class Home extends Component {
   render() {
