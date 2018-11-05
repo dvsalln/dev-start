5477396b793e815182f757cbebce047a2ae864bf
AAIO1992 <takahiro04kawaguchi13@gmail.com>
2018-11-05 01:47:58 -0800

    Removed redirect part again

--- server/routers/router.js
+++ server/routers/router.js
@@ -14,11 +14,11 @@ module.exports = function(app) {
   app.get("*", function(req, res, next) {
     res.sendFile(__dirname, "../client/build/index.html");
   });
-  app.get("/api", requireAuth);
+  // app.get("/api", requireAuth);
   // If the password / username combination is correct, give the user a token. If no, don't authenticate
-  app.post("/api/signin", requireSignin, Authentication.signin);
+  // app.post("/api/signin", requireSignin, Authentication.signin);
   // app.post("/api/createprofile", Authention.)
-  app.post("/api/signup", Authentication.signup);
+  // app.post("/api/signup", Authentication.signup);
   // Wire up API routes here!
   app.get("/api/developer/account", function(req, res, next) {
     developer
