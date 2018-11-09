2208e0344d8b567c630cb2030046616e8eb12052
arjunalapsapkota <arjunalapsapkota@gmail.com>
2018-11-08 17:50:38 -0800

    global Log-Out button added to the side drawer

--- client/src/components/Global/SideDrawer/SideDrawer.js
+++ client/src/components/Global/SideDrawer/SideDrawer.js
@@ -1,24 +1,26 @@
 import React from "react";
-import './SideDrawer.css';
-
+import "./SideDrawer.css";
+import LogOut from "../../LogOut";
 const SideDrawer = props => {
-    let drawerClasses = 'side-drawer';
-    if (props.show) {
-        drawerClasses = 'side-drawer open';
-    }
-    return (
+  let drawerClasses = "side-drawer";
+  if (props.show) {
+    drawerClasses = "side-drawer open";
+  }
+  return (
     <nav>
-        <ul className={drawerClasses}>
-              <a href="/developer/dashboard">Dev Dash</a>
-              <br></br>
-              <a href="/developer/profile">My Profile</a>
-              <br></br>
-              <a href="/sponsor/dashboard">Sponsor Dash</a>
-              <br></br>
-              <a href="/sponsor/profile">Sponsors</a>
-          </ul>
+      <ul className={drawerClasses}>
+        <a href="/developer/dashboard">Dev Dash</a>
+        <br />
+        <a href="/developer/profile">My Profile</a>
+        <br />
+        <a href="/sponsor/dashboard">Sponsor Dash</a>
+        <br />
+        <a href="/sponsor/profile">Sponsors</a>
+        <br />
+        <LogOut />
+      </ul>
     </nav>
-    )
+  );
 };
 
 export default SideDrawer;
