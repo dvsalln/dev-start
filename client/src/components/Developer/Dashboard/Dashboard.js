473228b4a4ccdd0d022cd9fd011795e174e38d22
Doc Ellis <DAVROGALL@ICLOUD.COM>
2018-11-29 16:43:55 -0800

    adds developer and sponsor modals on homepage

--- client/src/components/Developer/Dashboard/Dashboard.js
+++ client/src/components/Developer/Dashboard/Dashboard.js
@@ -35,7 +35,6 @@ class Dashboard extends Component {
     return (
       <div className="container-flex">
         <div style={{ height: "100%" }}>
-          <LogOut />
           <div className="wrapper">
             <div className="row dev-db-row">
               <Header drawerClickHandler={this.drawerToggleClickHandler} />
--- client/src/components/Home/DevModal/DevModal.js
+++ client/src/components/Home/DevModal/DevModal.js
@@ -1,44 +1,31 @@
-import React from "react";
+import React, { Component } from "react";
 import "./DevModal.css";
 
-const DevModal = () => (
-    <div id="id01" class="modal">
-      <span onclick="document.getElementById('id01').style.display='none'" class="close" title="Close Modal">&times;</span>
 
-     
-      <form class="modal-content animate" id="login" action="index.html">
-        <div class="imgcontainer">
-          <img src="assets/images/avatar.png" alt="Avatar" class="avatar">
-        </div>
-        <div class="container">
-          <div class="row">
-            <label for="uname">
-              <b>Email</b>
-            </label>
-            <input type="text" placeholder="Enter Email" name="uname" id="ln-email" required>
-          </div>
-
-          <div class="row">
-            <label for="psw">
-              <b>Password</b>
-            </label>
-            <input type="password" placeholder="Enter Password" name="psw" id="ln-password" required>
-          </div>
-
-          <div class="row">
-            <button type="submit">Login</button>
-            <label>
-              <input type="checkbox" checked="checked" name="remember">
-              <a style="color:black">Remember me</a>
-            </label>
-
-            <div class="row">
-              <button type="button" onclick="document.getElementById('id01').style.display='none'" class="cancelbtn">Cancel</button>
+class DevModal extends Component {
+ 
+    render() {
+        if (!this.props.show) {
+            return null;
+        }
+        return (
+            <div>
+                <div id="dev-modal">
+                <h3>DevStart Developers</h3>
+                <ul> 
+                    <li>Explore new freelance opportunities</li>
+                    <li>Build and create teams with other Devs</li>
+                    <li>Apply for job proposals posted by Sponsors</li>
+                    <li>Network and share new job listings or meetups</li>
+                    <li>Compete for a spot on the Leaderboard</li>
+                </ul>
+                <p>
+                    We at DevStart believe it is crucial to overall success as an Web Developer to be open to new ideas and opportunities, 
+                    and that begins with being prepared and well-equiped with the tools to do so. <br /><br /><h4>Find these tools<a href="/signup">here!</a></h4>
+                </p>
+                </div>
             </div>
-          </div>
-        </div>
-      </form>
-    </div>
-);
-
-export default DevModal
+        )
+    }
+}
+export default DevModal;
