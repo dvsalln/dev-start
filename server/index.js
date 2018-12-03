b655dcde21adadd3929fcccf95db9ae32dcf9970
arjunalapsapkota <arjunalapsapkota@gmail.com>
2018-12-02 22:02:29 -0800

    continue

--- server/index.js
+++ server/index.js
@@ -21,7 +21,7 @@ const mongoose = require("mongoose");
 const sqlDb = require("./sqlDatabase/models");
 
 // Import Middleware
-//const morgan = require("morgan"); // Helps us develop and debug locally
+const morgan = require("morgan"); // Helps us develop and debug locally
 const cors = require("cors"); // Helps us bypass CORS issues when developing locally
 const bodyParser = require("body-parser"); // Helps us extract features from req.body
 
@@ -47,10 +47,14 @@ class App {
           useFindAndModify: false
         } // Helps us avoid deprecation errors.
       );
-      sqlDb.sequelize.sync({ force: false }).then(function() {
-        console.log("SQL databse is connected");
-      });
 
+      try {
+        sqlDb.sequelize.sync({ force: false }).then(function() {
+          console.log("SQL databse is connected");
+        });
+      } catch (err) {
+        console.log("here ...... $$$$$ ############", err);
+      }
       // Put a Sequelize connection here... ex: this.sqldb = sequelize connection
 
       console.log("Successfully connected to database.");
@@ -59,13 +63,23 @@ class App {
     }
   }
   initMiddleware() {
-    //app.use(cors());
+    app.use(cors());
     app.use(bodyParser.json({ type: "*/*" })); // Type indicates ALL header types OK
     app.use(express.static(path.resolve(__dirname, "..", "..", "public"))); // Serve files in our Rect app public directory
     router(app);
     proposalRouter(app);
 
-    let PORT = process.env.PORT || 3080;
+    if (process.env.NODE_ENV === "production")
+      app.use(express.static("client/build"));
+    if (process.env.NODE_ENV === "test") app.use(morgan(() => null));
+    else
+      app.use(
+        morgan(
+          "API Request (port " +
+            this.port +
+            "): :method :url :status :response-time ms - :res[content-length]"
+        )
+      );
   }
   run() {
     this.server.listen(this.port, () => {
